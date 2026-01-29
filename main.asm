8277fa70968e7576a8fe09a612dbde0df6bfa058
MaxProgramm <89091446+MaxProgramm@users.noreply.github.com>
2026-01-29 11:38:15 +0100

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,95 +1,201 @@
-; Systemvorbereitungen
     .cdecls C,LIST,"msp430.h"
-	.include lib.include
-_main	init
-; Ende Systemvorbereitungen
+    .include lib.include
+
+_main   init
+        jmp reset
 
 
-; Beginn des Programms
+; =========================================================
+; start, setzten von start werten
+; =========================================================
 start
-	mov.b #2, &520
-	mov.b #3, &521
-	mov.b #1, &522
-	mov.b #4, &523
-	mov #0, &33
-	mov #0, r12
-end
+    mov.b   #1, &520
+    mov.b   #2, &521
+    mov.b   #4, &522
+    mov.b   #8, &523
+    mov.b   #2, &524
+    mov.b   #1, &525
+    mov.b   #8, &526
+    mov.b   #4, &527
+    mov.b   #1, &528
+    mov.b   #2, &529
+    ret
 
+
+; ========================================================= 
+; reset
+; =========================================================
 reset
-	mov #520, r12 ;//first level address
-	mov #520, r13 ;//current level address
-	;start_reg = 513 // müsste 512 sein
-	;current_reg = 513
-	jmp game_loop
- 
+    call    #start ;call dass wir mit ret hierher zurückspringen
+    mov     #520, r12
+    mov     #520, r13
+    mov.b   #0, &33
+    jmp     game_loop
+
+
+; =========================================================
+; loop
+; =========================================================
 game_loop
-	jmp show_leds
- 
- 
+    call    #show_leds 
+    call    #check_btn_order
+    jmp     game_loop
+
+
+; =========================================================
+; leds zeigen für entsprechendes level
+; =========================================================
 show_leds
-	mov r12, r14 ;//Adress 514 contains the address for the rounds inner level
+    mov     r12, r14
 
-loop_lights
-	mov #0, &33
-	mov.b 0(r14), &33
-	sleep
-	cmp r13, r14
-	;sleep
-	jn loop_lights_helper
-	;// oben implementiert
-	;while &514 = 513
-	;
-	;	&33 = 0(514)
-	;	&514 + 1
-	;
-	jmp check_btn_order
- 
- 
-check_btn_order:
-	;button checken
-	mov r12, r14
-loop_buttons
-	;//ckecks if the current inner round level (&514) is higher than the current general level (&513), if yes jump to level up
-	cmp r14, r13
-	jn level_up
-	;//muss verbessert werden 
-	;sleep
-	;//checken ob die aktuell gedrückte taste, dem Wert aus dem aktuellen Level entspricht
-	mov.b #15, r11
-	sub.b &40, r11
-	cmp.b r11, 0(r14)
-	
-	jz loop_buttons_helper
-	;//if pressed button not equal to current level
-	jmp reset
- 
+show_leds_loop
+    mov.b   0(r14), r10
+    and.b   #00001111b, r10      
+    mov.b   r10, &33
+    sleep
+    mov.b   #0, &33
+    sleep
+
+    cmp     r13, r14
+    jeq     show_leds_done
+    inc     r14
+    jmp     show_leds_loop
+
+show_leds_done
+    ret
+
+
+; =========================================================
+; check btn order
+; =========================================================
+check_btn_order
+    mov     r12, r14
+
+check_loop
+    call    #get_button_stable
+
+    cmp.b   r11, 0(r14)
+    jne     lose
+
+    call    #ok_flash_led
+
+    cmp     r13, r14
+    jeq     level_up
+
+    inc     r14
+    jmp     check_loop
+
+
+; =========================================================
+; Verloren und reset
+; =========================================================
+lose
+    call    #anim_fail_all 
+    jmp     reset
+
+
+; =========================================================
+; Level erhöhen und bei höher als 10 reset
+; =========================================================
 level_up
-	inc r13
-	jmp game_loop
- 
- 
- 
-	;wenn richtig
-	;	&513 + 1
-	;	jump game_loop
-	;wenn falsch
-	;	jump reset
- 
-loop_buttons_helper
-	inc r14
-	jmp loop_buttons
-
-loop_lights_helper
-	inc r14
-	jmp loop_lights
+    call    #anim_success_all
+    inc     r13
+    cmp     #530, r13             
+    jeq     reset
+    ret
 
 
+; =========================================================
+; Feedback funktion auf led8 bei richtigem press
+; =========================================================
+ok_flash_led
+    mov.b   #10000000b, &33
+    sleep       
+    mov.b   #00000000b, &33
+    ret
 
 
+; =========================================================
+;liest buttons und wartet kurz bis stabiles ergebnis, dann in register r11
+; =========================================================
+get_button_stable
+wait_press
+    mov.b   &40, r11
+    and.b   #00001111b, r11
+    cmp.b   #00001111b, r11          
+    jeq     wait_press
+
+    sleep
+    mov.b   &40, r10
+    and.b   #00001111b, r10
+    cmp.b   r11, r10
+    jne     wait_press               
+
+    xor.b   #00001111b, r11
+
+    cmp.b   #1, r11
+    jeq     ok_btn
+    cmp.b   #2, r11
+    jeq     ok_btn
+    cmp.b   #4, r11
+    jeq     ok_btn
+    cmp.b   #8, r11
+    jeq     ok_btn
+
+    jmp     wait_press         
+
+ok_btn
+wait_release
+    mov.b   &40, r10
+    and.b   #00001111b, r10
+    cmp.b   #00001111b, r10
+    jne     wait_release
+
+    sleep
+    ret
 
 
+; =========================================================
+; Success animation (1x flash)
+; =========================================================
+anim_success_all
+    mov     #2, r6
+as_loop
+    mov.b   #0FFh, &33
+    sleep
+    mov.b   #00h, &33
+    sleep
+    dec     r6
+    jnz     as_loop
+    ret
 
 
-; Ende des Programms
+; =========================================================
+; Fail animation (flash und 1x lauflicht)
+; =========================================================
+anim_fail_all
+    mov.b   #0FFh, &33
+    call    #_sleep
+    mov.b   #00h, &33
+    call    #_sleep 
 
-	.include vectors.include
+    mov.b   #00000001b, &33
+    sleep
+    mov.b   #00000010b, &33
+    sleep
+    mov.b   #00000100b, &33
+    sleep
+    mov.b   #00001000b, &33
+    sleep
+    mov.b   #00010000b, &33
+    sleep
+    mov.b   #00100000b, &33
+    sleep
+    mov.b   #01000000b, &33
+    sleep
+    mov.b   #10000000b, &33
+    sleep
+    mov.b   #00000000b, &33
+    ret
+
+    .include vectors.include
